c9404d961beddc456767ea2cb8730262eafb7f69
Kh Md Shahnayas <shahnayas01@gmail.com>
2021-08-08 11:25:48 +0600

    Update hotel.sql

--- MidTerm-CSE-0318/onlinehotelreservation/hotel.sql
+++ MidTerm-CSE-0318/onlinehotelreservation/hotel.sql
@@ -1,20 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 18, 2017 at 05:44 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `id1709610_hotel`
