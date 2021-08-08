c8ff2e58a8b71bcd301a395b8640061062d670db
Kh Md Shahnayas <shahnayas01@gmail.com>
2021-08-08 11:13:55 +0600

    Add files via upload

---
+++ MidTerm-CSE-0318/onlinehotelreservation/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Online Hotel Reservation System</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .well
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+    
+        }
+        .wellfix
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+            height: 150px;
+        }
+		body
+		{
+			background-image: url('images/intercontinental-dhaka.jpg');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
+		p
+		{
+			font-size: 13px;
+		}
+        .pro_pic
+        {
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            margin-bottom: 15px;
+            margin-right: 15px;
+        }
+		
+    </style>
+  
+</head>
+
+<body>
+    <div class="container">
+      
+       <img class="img-responsive" src="images/intlogo.jpg" style="width:100%; height:180px;">      
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="room.php">Book Room</a></li>
+                    <li><a href="admin.php">Admin</a></li>
+                </ul>
+          </div>
+        </nav>
+
+        <div class="jumbotron">
+        <div class="w3-content w3-section">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/1.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/2.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/3.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/4.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/5.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/6.jpg" style="width:100%; height:450px;">
+        </div>    
+        </div>
+        <hr>
+        <div class="row" style="color: #ed9e21">
+            <div class="col-md-12 well" >
+              <h4><strong style="color: #ffbb2b">About</strong></h4><br>
+              <p>Online hotel reservations are a popular method for booking hotel rooms. Travelers can book rooms on a computer by using online security to protect their privacy and financial information and by using several online travel agents to compare prices and facilities at different hotels</p>
+              <br>
+              <p></p>
+              <br>
+              <p></p>
+              
+            </div>  
+        </div>
+        <div class="row" style="color: #ffbb2b">
+            <div class="col-md-4 wellfix">
+              <h4><strong>Contact Us</strong></h4><hr>
+              Address : G, 1 Minto Rd, Dhaka 1000<br>
+              Mail : shahnayas01@gmail.com <br>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 wellfix">
+                <h4><strong>Developed By</strong></h4><hr>
+                <a href="#"></a>
+<p> <a href="https://github.com/Shahnayas">Kh MD Shahnayas </a> Github Link: https://github.com/Shahnayas </p>
+            </div>
+        </div>
+     
+    </div>
+    
+    <script src="my_js/slide.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
